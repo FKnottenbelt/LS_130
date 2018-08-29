f6d093d37c776b3fd440bfe5cf28fa21869dc66f
Femke Knottenbelt <femknotgithub@gmail.com>
2018-08-29 09:46:35 +0000

    Add example end and start of line

---
+++ regex/ex_11.rb
@@ -0,0 +1,3 @@
+TEXT1 = "red fish\nblue fish"
+puts "matched red" if TEXT1.match(/^red/)
+puts "matched blue" if TEXT1.match(/^blue/)
---
+++ regex/ex_12.rb
@@ -0,0 +1,3 @@
+TEXT2 = "red fish\nred shirt"
+puts "matched fish" if TEXT2.match(/fish$/)
+puts "matched shirt" if TEXT2.match(/shirt$/)
