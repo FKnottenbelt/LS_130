76a758f03d906dc3412997c7ee23af882bbfd9da
Femke Knottenbelt <femknotgithub@gmail.com>
2018-09-01 17:32:05 +0000

    Add example Yielding with an argument

---
+++ lesson_1/04_methods_with_blocks/example_06.rb
@@ -0,0 +1,16 @@
+# Yielding with an argument
+
+# method implementation
+def increment(number)
+  if block_given?
+    yield(number + 1)
+  end
+  number + 1
+end
+
+# method invocation
+increment(5)  # => 6
+
+increment(5) do |num|
+  puts num
+end
--- review.md
+++ review.md
@@ -1,2 +1,3 @@
 # talking about code
 01-04 'Passing execution to the block'
+01-04 'Yielding with an argument'
