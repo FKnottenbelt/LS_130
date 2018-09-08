7aa7a115759fd295daf5202ca78b984279f90901
Femke Knottenbelt <femknotgithub@gmail.com>
2018-09-08 18:04:37 +0000

    Add upt to test_shift

--- lesson_2_testing/07_todolist_testsuite/todolist_test.rb
+++ lesson_2_testing/07_todolist_testsuite/todolist_test.rb
@@ -6,7 +6,7 @@ require_relative 'todolist'
 
 class TodoListTest < MiniTest::Test
 
-  def setup
+  def setup # will set up before every test (every one start 'clean')
     @todo1 = Todo.new("Buy milk")
     @todo2 = Todo.new("Clean room")
     @todo3 = Todo.new("Go to gym")
@@ -18,6 +18,28 @@ class TodoListTest < MiniTest::Test
     @list.add(@todo3)
   end
 
-  # Your tests go here. Remember they must start with "test_"
+  def test_to_a
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_size
+    assert_equal(3, @list.size)
+  end
+
+  def test_first
+    assert_equal(@todo1, @list.first) # [ ] Buy milk == [ ] Buy milk
+  end
+
+  def test_last
+    assert_equal(@todo3, @list.last)
+  end
+
+  def test_shift
+    first = @list.shift
+    assert_equal(@todo1, first)
+    assert_equal(2, @list.size )
+    assert_equal([@todo2, @todo3], @list.to_a) # just in case
+  end
 
 end
+
