c8246e4f3310ee68f3ff94cd2d44ec603178b769
Femke Knottenbelt <femknotgithub@gmail.com>
2018-09-09 09:22:25 +0000

    Add upto test_item_at

--- lesson_2_testing/07_todolist_testsuite/todolist_test.rb
+++ lesson_2_testing/07_todolist_testsuite/todolist_test.rb
@@ -41,5 +41,57 @@ class TodoListTest < MiniTest::Test
     assert_equal([@todo2, @todo3], @list.to_a) # just in case
   end
 
+  def test_pop
+    last = @list.pop
+    assert_equal(@todo3, last)
+    assert_equal(2, @list.size)
+    assert_equal([@todo1, @todo2], @list.to_a)
+  end
+
+  def test_done?
+    @todo1.done = true
+    @todo3.done = true
+    todos = @list.instance_variable_get("@todos")
+    no_of_dones = todos.map(&:done).select{ |action| action == true }.count
+    assert_equal(2, no_of_dones)
+  end
+
+  def test_done_question # ls version of test_done?
+    assert_equal(false, @list.done?)
+  end
+
+  def test_typeerror_when_adding_non_todo_object
+    assert_raises(TypeError) {@list.add(2)}
+    assert_raises(TypeError) { @list.add('hi') }
+  end
+
+  def test_shovel_adds_todo_to_list
+    todo4 = Todo.new("Test << method")
+    @list << todo4
+    todos = @list.instance_variable_get("@todos")
+    assert_same(todo4, todos.last)
+  end
+
+  def test_shovel # ls version of <<
+    new_todo = Todo.new("Walk the dog")
+    @list << new_todo
+    @todos << new_todo
+
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_add_alias
+    new_todo = Todo.new("Test add alias for <<")
+    @list.add(new_todo)
+    @todos << new_todo
+
+    assert_equal(@todos, @list.to_a)
+  end
+
+  def test_item_at
+    assert_raises(IndexError){ @list.item_at(300) }
+    assert_same(@todo1, @list.first)
+  end
 end
+
 
