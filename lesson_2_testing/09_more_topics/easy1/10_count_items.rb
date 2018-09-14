a341c0b11a455e94f8f5f2f5e62feaa6c453c8e2
Femke Knottenbelt <femknotgithub@gmail.com>
2018-09-14 06:52:44 +0000

    Add exercise 02-09-10

---
+++ lesson_2_testing/09_more_topics/easy1/10_count_items.rb
@@ -0,0 +1,57 @@
+=begin
+
+Count Items
+
+Write a method that takes an array as an argument, and a block
+that returns true or false depending on the value of the array
+element passed to it. The method should return a count of the
+number of times the block returns true.
+
+You may not use Array#count or Enumerable#count in your solution.
+
+Examples:
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
+=end
+
+def count(array)
+  count = 0
+  array.each do |element|
+    count += 1 if yield(element)
+  end
+  count
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
+
+=begin
+Further Exploration
+
+Write a version of the count method that meets the conditions
+of this exercise, but also does not use each, loop, while, or
+until.
+
+==
+def count(array)
+  array.reduce(0) { |sum, el| yield(el) ? sum + 1 : sum }
+end
+==
+def count(collection)
+ reason = []
+ collection.length.times do |count|
+   reason.push(yield(collection[count]))
+ end
+ p reason.select {|x| x == true}.length
+end
+
+=end
