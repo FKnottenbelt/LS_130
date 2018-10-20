2317f0ff15e711de86cfba868dbb955c38055cd6
Femke Knottenbelt <femknotgithub@gmail.com>
2018-10-20 10:40:07 +0000

    Set up odd words

---
+++ lesson_2_testing/12_challenges/easy2/01_odd_words.rb
@@ -0,0 +1,30 @@
+=begin
+
+Odd Words and the How to Work with a Code Challenge
+
+We did a live problem solving session and used the "Odd Words" problem to
+talk about the general process of working through a live code challenge.
+
+Unlike other challenges, this one doesn't come with a complete test suite,
+so you have to work out your own examples(you don't have to write the
+actual tests, just example inputs and outputs). You can watch the first
+10 minutes of the video on the general process and the problem itself,
+try to work on the problem, and fast foward to 47:20 or so when we discuss
+the solutions.
+
+problem:
+consider a character set consisting of letters, a space, and a point
+Words consist of one or more, but at most 20 letters. An input text
+consists of one or more words sperated from each other by one or more
+spaces and terminated by 0 or more spaces followed by a point. The input
+should be read from, and including, the first letter of the first word,
+up to and including the terminating point. The output text is to be
+produced such that succesive words are seperated by a single space with
+the last word being terminiated by a single point. Odd words are copied
+in reverse order while even words are merely echoed. For example, the
+input string
+'whats the matter with kansas.' becomes 'whats eht matter htiw kansas.'
+
+bonus points: the characters must be read and printed one at a time.
+=end
+
